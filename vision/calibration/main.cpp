ad0282433cc0fb7ece6ea1c8fb2aa6f396e1867a
Osvaldoav <jorgeosvaldoav@gmail.com>
2019-04-03 23:56:43 -0600

    new input handeler added

--- vision/calibration/main.cpp
+++ vision/calibration/main.cpp
@@ -1,43 +1,43 @@
 #include <opencv2/imgproc.hpp>  // hsv
 #include "Calibration.h"
 
-// Validates input 
-bool isNotValid(int &argc, char** &argv, cv::Mat &image){
-  if( argc != 2){
-    std::cout <<"Usage: ./main test.png" << std::endl;
-    return true;
-  }
-  // Read the file
-  std::string path = "../images/" + std::string(argv[1]);
-  image = cv::imread(path, CV_LOAD_IMAGE_COLOR);
-
-  // Check for invalid input
-  if(! image.data ){                             
-    std::cout <<  "Could not open or find the image" << std::endl ;
-    return true;
+// Validates input. True if static media / False if not
+bool inputValidation(int &argc, char** &argv, cv::Mat &image, cv::VideoCapture &cap){
+  std::string path;
+  
+  if(argc > 1){
+    path = "../media/" + std::string(argv[1]);
+    if(int(std::string(argv[1]).find(".png")) > -1){
+      image = cv::imread(path, CV_LOAD_IMAGE_COLOR);
+      return false;
+    }
   }
 
-  return false;
+  argc > 1 && std::string(argv[1]).find(".mp4") ? cap.open(path) : cap.open(0);
+  cap >> image;
+  return true;
 }
 
 int main(int argc, char** argv){
+  std::cout <<"Usage(static video): ./main test.mp4" << std::endl;
+  std::cout <<"Usage(static image): ./main test.png" << std::endl;
+  std::cout <<"Usage(video capture): ./main" << std::endl;
+
   cv::Mat image;
-
-  // Validates the input image
-  if(isNotValid(argc, argv, image))
+  cv::VideoCapture cap;
+  bool stat = inputValidation(argc, argv, image, cap);
+  if(! image.data){
+    std::cout <<  "Could not open or find the image/video" << std::endl ;
     return -1;
-  // cv::resize(image, image, cv::Size(), 0.46, 0.46);
-
-  // Initialize video and stores it in 'image'
-  // cv::VideoCapture cap(argc > 1 ? atoi(argv[1]) : 0);
-  // cap >> image;
+  }
 
   // Initialize calibration
   Calibration("Calibration", image);
 
   while(true){
     // Updates the image taken by the camera
-    // cap >> image;
+    if(stat)
+      cap >> image;
     // cv::resize(image, image, cv::Size(), 0.6, 0.6);
 
     // Exit program if no image was found
