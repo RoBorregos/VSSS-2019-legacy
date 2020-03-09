08449ded51267087af57766b815b117608739697
zJuarez <mji2000.741@gmail.com>
2020-03-09 16:05:50 -0600

    Update main.cpp

--- Simulator/src/main.cpp
+++ Simulator/src/main.cpp
@@ -1,183 +1,208 @@
-#include <cmath>
+/*
+ * This file is part of the VSS-SampleStrategy project.
+ *
+ * This Source Code Form is subject to the terms of the GNU GENERAL PUBLIC LICENSE,
+ * v. 3.0. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
+ */
+
+#include <Communications/StateReceiver.h>
 #include <Communications/CommandSender.h>
 #include <Communications/DebugSender.h>
-#include <Communications/StateReceiver.h>
-#include <future>
+#include "cstdlib"
 #include <math.h>
-#include <stack>
-#include <tuple>
-#include <unistd.h>
-#include <utility>
-#include <vector>
 
-#include "../../libraries/a_star/a_star.h"
+static const double pi = 3.141592;
+using namespace vss;
 
-#define MAX_MOTOR_VELOCITY 10
-#define SAFE_ZONE 5
-#define TEAM_BLUE 0
-#define TEAM_YELLOW 1
+IStateReceiver *stateReceiver;
+ICommandSender *commandSender;
+IDebugSender *debugSender;
 
-#define BOTTOM_LEFT_CORNER std::pair<int, int>(0, 130)
-#define BOTTOM_RIGHT_CORNER std::pair<int, int>(170, 130)
-#define TOP_LEFT_CORNER std::pair<int, int>(0, 0)
-#define TOP_RIGHT_CORNER std::pair<int, int>(170, 0)
+State state;
 
-/*
-    Angle:
-                270
-                 ^
-                 |
-       180  <-       ->  0
-                 |
-                 v
-                90
+double wrapMax(double x, double max)
+{return fmod(max+fmod(x,max),max);}
+double wrapMinMax(double x, double min, double max)
+{return min+wrapMax(x-min,max-min);}
+double distance(double x, double y,double x2, double y2)
+{return 0.0113*sqrt((x-x2)*(x-x2)+(y-y2)*(y-y2));}
 
-    Coordinate of important points
+double s2 =100;
+double s1 = 500;
+int min = 15;
 
-    Crosses:
-        Down right: 122, 104
-        Up right: 122, 25
-        Up left: 48, 25
-        Down left: 48, 104
+struct position{
+double x;
+double y;
+double angle;
+double speed;
+double Aspeed;
 
-    Goal:
-        Right goal: 
-            Down: 155, 81
-            Up: 155, 49
-        Left goal: 
-            Down: 15, 81
-            Up: 15, 49
-    
-    Center: 85, 65
+position(double x_,
+double y_){
+x = x_;
+y=-1.0*y_;
+angle = 0;
+speed = 5;
+Aspeed = 1;
+}
+position(int id){
+x = state.teamYellow[id].x;
+y=-1.0*state.teamYellow[id].y;
+angle = ((int)(360-state.teamYellow[id].angle)%360)*pi/180.0;
+angle = wrapMinMax(angle,-pi,pi);
+speed = 0.0113*sqrt(state.teamYellow[id].speedX*state.teamYellow[id].speedX+state.teamYellow[id].speedY*state.teamYellow[id].speedY);
+Aspeed = 1;
+}
+void print(){
+std::cout<<"Pos X: "<<x<<std::endl;
+std::cout<<"Pos Y: "<<y<<std::endl;
+std::cout<<"Angle: "<<angle<<std::endl;
+std::cout<<"Speed: "<<speed<<std::endl;
+std::cout<<"Angular Speed: "<<Aspeed<<std::endl;
+}
+position(char p)
+{
+x = state.ball.x;
+y=-1.0*state.ball.y;
+angle = 0;
+speed = 5;
+Aspeed = 0;
+}
+};
 
-*/
+struct param{
+double ts=0.01;
+double k[3] = {3.5,4.5,7.1};
+double r=0.016;
+double l = 0.062;
+}rob;
 
-vss::IStateReceiver *state_receiver;
-vss::ICommandSender *command_sender;
-vss::IDebugSender *debug_sender;
+void send_commands(std::vector<std::pair<double,double>>);
+void send_debug();
+std::pair<double,double> calculate(position Cur, position Ref);
+double map(double val,double fromL, double fromH, double toL, double toH)
+{return (val-fromL)*(toH-toL)/(fromH-fromL)+toL;}
 
-void printData() {
-    auto states = state_receiver->receiveState(
-      vss::FieldTransformationType::None);
 
-    std::cout << states << std::endl;
+void moveTo(int id, double x, double y, std::vector<std::pair<double,double>> &velocities)
+{
+position current (id);
+position reference (x,y);
+min = id==0? 2: 15;
+std::pair<double,double> result = calculate(current,reference);
+velocities[id] = result;
+std::cout<<id<<": "<<result.first<<" "<<result.second<<std::endl;
+
 }
 
+std::vector <std::pair<double,double>> velocities;
+
 int main(int argc, char** argv){
-    state_receiver = new vss::StateReceiver();
-    command_sender = new vss::CommandSender();
-    debug_sender = new vss::DebugSender();
+    srand(time(NULL));
 
-    state_receiver->createSocket();
-    command_sender->createSocket(vss::TeamType::Yellow);
-    debug_sender->createSocket(vss::TeamType::Yellow);
+    stateReceiver = new StateReceiver();
+    commandSender = new CommandSender();
+    debugSender = new DebugSender();
 
+    stateReceiver->createSocket();
+    commandSender->createSocket(TeamType::Yellow);
+    debugSender->createSocket(TeamType::Yellow);
+    //position ref (85,65,180,5,0);
 
-    std::vector<std::pair<int, int>> corners;
-    corners.push_back(TOP_LEFT_CORNER);
-    corners.push_back(TOP_RIGHT_CORNER);
-    corners.push_back(BOTTOM_RIGHT_CORNER);
-    corners.push_back(BOTTOM_LEFT_CORNER);
+    while(true){
+        state = stateReceiver->receiveState(FieldTransformationType::None);
+        velocities = {std::make_pair(0,0),std::make_pair(0,0),std::make_pair(0,0)};
+        // moveTo's (robot id, x, y, velocities);
+       
+        moveTo(0, 158,state.ball.y<46? 46:state.ball.y>86? 86: state.ball.y,velocities);
+        moveTo(1, state.ball.x,state.ball.y,velocities);
+        moveTo(2, state.ball.x,state.ball.y,velocities);
 
-    a_star::AStar a_star_map;
-    while (true) {
-        auto states = state_receiver->receiveState(
-          vss::FieldTransformationType::None);
-        std::cout << states << std::endl;
-        std::tuple<a_star::Robot, int, int> goalie_coordinate = std::make_tuple(
-          a_star::Robot::GOALIE, states.teamYellow.at(0).x,
-          states.teamYellow.at(0).y);
-        std::tuple<a_star::Robot, int, int> defender_coordinate = 
-          std::make_tuple(a_star::Robot::DEFENDER, states.teamYellow.at(1).x, 
-          states.teamYellow.at(1).y);
-        std::tuple<a_star::Robot, int, int> mid_fielder_coordinate = 
-          std::make_tuple(a_star::Robot::MID_FIELDER, 
-          states.teamYellow.at(2).x, states.teamYellow.at(2).y);
-        std::pair<int, int> ball_coordinate = std::make_pair(
-          states.ball.x, states.ball.y);
-        std::tuple<a_star::Robot, int, int> enemy_goalie_coordinate = 
-          std::make_tuple(a_star::Robot::UNDEFINED, states.teamBlue.at(0).x, 
-          states.teamBlue.at(0).y);
-        std::tuple<a_star::Robot, int, int> enemy_defender_coordinate = 
-          std::make_tuple(a_star::Robot::UNDEFINED, states.teamBlue.at(1).x, 
-          states.teamBlue.at(1).y);
-        std::tuple<a_star::Robot, int, int> enemy_mid_fielder_coordinate = 
-          std::make_tuple(a_star::Robot::UNDEFINED, states.teamBlue.at(2).x, 
-          states.teamBlue.at(2).y);
-
-        a_star_map.CreateMap(std::vector<std::tuple<a_star::Robot, int, int>> {
-          goalie_coordinate, defender_coordinate, mid_fielder_coordinate, 
-          enemy_goalie_coordinate, enemy_defender_coordinate, 
-          enemy_mid_fielder_coordinate}, corners, 
-          SAFE_ZONE);
-
-        vss::Debug debug;
-        bool founded_path = a_star_map.AStarSearch(a_star::Robot::GOALIE,
-          std::make_pair(std::get<1>(goalie_coordinate), 
-          std::get<2>(goalie_coordinate)), ball_coordinate);
-        if (founded_path) {
-            vss::Path debug_path;
-            auto path = a_star_map.GetPath(std::make_pair(
-              std::get<1>(goalie_coordinate), std::get<2>(goalie_coordinate)), 
-              ball_coordinate);
-            while (!path.empty()) {
-                auto next_coordinate = path.top();
-                debug_path.points.push_back(vss::Point(next_coordinate.first,
-                    next_coordinate.second));
-                path.pop();
-            }
-            debug.paths.push_back(debug_path);
-        } else {
-            std::cout << "Path not found" << std::endl;
-        }
-        founded_path = a_star_map.AStarSearch(a_star::Robot::DEFENDER,
-          std::make_pair(std::get<1>(defender_coordinate), 
-          std::get<2>(defender_coordinate)), ball_coordinate);
-        if (founded_path) {
-            vss::Path debug_path;
-            auto path = a_star_map.GetPath(std::make_pair(
-              std::get<1>(defender_coordinate), 
-              std::get<2>(defender_coordinate)), ball_coordinate);
-            while (!path.empty()) {
-                auto next_coordinate = path.top();
-                debug_path.points.push_back(vss::Point(next_coordinate.first,
-                    next_coordinate.second));
-                path.pop();
-            }
-            debug.paths.push_back(debug_path);
-        } else {
-            std::cout << "Path not found" << std::endl;
-        }
-        founded_path = a_star_map.AStarSearch(a_star::Robot::MID_FIELDER,
-          std::make_pair(std::get<1>(mid_fielder_coordinate), 
-          std::get<2>(mid_fielder_coordinate)), ball_coordinate);
-        if (founded_path) {
-            vss::Path debug_path;
-            auto path = a_star_map.GetPath(std::make_pair(
-              std::get<1>(mid_fielder_coordinate), 
-              std::get<2>(mid_fielder_coordinate)), 
-              ball_coordinate);
-            while (!path.empty()) {
-                auto next_coordinate = path.top();
-                debug_path.points.push_back(vss::Point(next_coordinate.first,
-                    next_coordinate.second));
-                path.pop();
-            }
-            debug.paths.push_back(debug_path);
-        } else {
-            std::cout << "Path not found" << std::endl;
-        }
-
-        vss::Command command;
-        command.commands.push_back(vss::WheelsCommand(MAX_MOTOR_VELOCITY, 
-          MAX_MOTOR_VELOCITY));
-        command.commands.push_back(vss::WheelsCommand(MAX_MOTOR_VELOCITY, 
-          MAX_MOTOR_VELOCITY));
-        command.commands.push_back(vss::WheelsCommand(MAX_MOTOR_VELOCITY, 
-          MAX_MOTOR_VELOCITY));
-        command_sender->sendCommand(command);
-        debug_sender->sendDebug(debug);
+       send_commands(velocities);
+        
     }
 
     return 0;
+}
+
+void send_commands(std::vector<std::pair<double,double>> vel){
+    Command command;
+   
+   for(int i = 0; i<3; i++)
+command.commands.push_back(WheelsCommand(vel[i].first, vel[i].second));
+    
+    commandSender->sendCommand(command);
+}
+
+std::pair<double,double> calculate(position Cur, position Ref){
+
+
+double difX = Ref.x-Cur.x;
+double difY = Ref.y-Cur.y;
+
+if(difX==0 && difY==0)
+Ref.angle = 0;
+else if(difX==0)
+Ref.angle = pi/2.0;
+else 
+Ref.angle = atan(difY/difX);
+
+if(Cur.x>Ref.x)
+Ref.angle += pi;
+
+Ref.angle=wrapMinMax(Ref.angle,-pi,pi);
+
+//Ref.speed = Cur.speed<0? -5: 5;
+Ref.x += rob.ts*Ref.speed*cos(Ref.angle);
+Ref.y += rob.ts*Ref.speed*sin(Ref.angle);
+
+difX = Ref.x - Cur.x;
+difY = Ref.y - Cur.y;
+
+double e1 = 0.0113*difX*(cos(Ref.angle)+sin(Ref.angle));
+double e2 = 0.0113*difY*(cos(Ref.angle)-sin(Ref.angle));
+double e3 = wrapMinMax(Ref.angle-Cur.angle,-pi,pi);
+
+double v = Ref.speed*cos(e3)+rob.k[0]*e1;
+double w = Ref.Aspeed+rob.k[1]*Ref.speed*e2*+rob.k[2]*Ref.speed*sin(e3);
+
+Cur.speed = v;
+Cur.Aspeed = w;
+
+double right = (Cur.speed*2.0+Cur.Aspeed*rob.l)/(2.0*rob.r);
+double left = (Cur.speed*2.0-Cur.Aspeed*rob.l)/(2.0*rob.r);
+        
+        right = map(right,-s1,s1,-s2,s2);
+        left = map(left,-s1,s1,-s2,s2);
+
+
+        if(right>-min && right<min)
+        right = right<0? -min: min;
+
+        if(left>-min && left<min)
+        left = left<0? -min: min;
+        
+    return std::make_pair(right,left);
+}
+
+void send_debug(){
+    vss::Debug debug;
+
+    for(unsigned int i = 0 ; i < 3 ; i++){
+        debug.stepPoints.push_back(Point(85 + rand()%20, 65 + rand()%20));
+    }
+
+    for(unsigned int i = 0 ; i < 3 ; i++){
+        debug.finalPoses.push_back(Pose(85 + rand()%20, 65 + rand()%20, rand()%20));
+    }
+
+    for(unsigned int i = 0 ; i < 3 ; i++){
+        vss::Path path;
+        path.points.push_back(Point(85, 65));
+        path.points.push_back(Point(85 + rand()%20, 65 + rand()%20));
+    }
+
+    debugSender->sendDebug(debug);
 }
