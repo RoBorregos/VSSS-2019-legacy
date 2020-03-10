5e777b26bcc6ba09e32c57411de9b4e11d8b5f30
zJuarez <mji2000.741@gmail.com>
2020-03-10 17:02:25 -0600

    conf both teams
    yhlqmdlg

--- Simulator/src/main.cpp
+++ Simulator/src/main.cpp
@@ -13,7 +13,7 @@
 #include <math.h>
 #include <chrono>
 
-static const double pi = 3.141592;
+
 using namespace vss;
 
 IStateReceiver *stateReceiver;
@@ -23,6 +23,8 @@ ICommandSender *commandSenderB;
 IDebugSender *debugSenderB;
 
 State state;
+
+static const double pi = 3.141592;
 
 double wrapMax(double x, double max)
 {
@@ -37,10 +39,6 @@ double distance(double x, double y, double x2, double y2)
     return 0.0113 * sqrt((x - x2) * (x - x2) + (y - y2) * (y - y2));
 }
 
-double s2 = 105;
-double s1 = 500;
-int min = 15;
-
 struct param
 {
     double ts = 0.01;
@@ -49,12 +47,15 @@ struct param
     std::chrono::time_point<std::chrono::system_clock> before[6];
     double r = 0.016;
     double l = 0.062;
+    double s2 = 105;
+    double s1 = 500;
+    int min = 15;
     double distanceA[6] = {0, 0, 0, 0, 0, 0};
 } rob;
 
 void send_commands(std::vector<std::pair<double, double>>, std::vector<std::pair<double, double>>);
 void send_debug();
-void moveTo(std::string  team, int id, double x, double y, double angle, std::vector<std::pair<double,double>> &v))
+void moveTo(std::string  team, int id, double x, double y, double angle, std::vector<std::pair<double,double>> &v);
 std::pair<double,double> calculate(std::string team, int id, double x, double y, double angle);
 
 double map(double val, double fromL, double fromH, double toL, double toH)
@@ -64,7 +65,7 @@ double map(double val, double fromL, double fromH, double toL, double toH)
 
 void moveTo(std::string team, int id, double x, double y, double angle, std::vector<std::pair<double, double>> &v)
 {
-    min = id == 0 ? 2 : 15;
+    rob.min = id == 0 ? 2 : 15;
     std::pair<double, double> result = calculate(team, id, x, y, angle);
     v[id] = result;
 }
@@ -75,24 +76,24 @@ std::pair<double, double> calculate(std::string team, int id, double x, double y
     double REF_X, REF_Y, CUR_X, CUR_Y, REF_ANGLE, CUR_ANGLE, REF_SPEED, REF_ASPEED;
     if(team=="yellow"){
     CUR_X = state.teamYellow[id].x;
-    CUR_Y = state.teamYellow[id].y;
+    CUR_Y = -state.teamYellow[id].y;
     CUR_ANGLE = ((int)(360 - state.teamYellow[id].angle) % 360) * pi / 180.0;
     }
     else{
     CUR_X = state.teamBlue[id].x;
-    CUR_Y = state.teamBlue[id].y;
+    CUR_Y = -state.teamBlue[id].y;
     CUR_ANGLE = ((int)(360 - state.teamBlue[id].angle) % 360) * pi / 180.0;
     }
 
     CUR_ANGLE = wrapMinMax(CUR_ANGLE, -pi, pi);
 
     REF_X = x;
-    REF_Y = y * -1;
+    REF_Y = -y;
 
     double difX = REF_X - CUR_X;
     double difY = REF_Y - CUR_Y;
 
-    if (REF_ANGLE > -10)
+    if (angle > -500)
     {
         REF_ANGLE = ((int)(360 - angle) % 360) * pi / 180.0;
     }
@@ -110,7 +111,11 @@ std::pair<double, double> calculate(std::string team, int id, double x, double y
             REF_ANGLE += pi;
     }
 
+    double d = distance(REF_X, REF_Y, CUR_X, CUR_Y);
     REF_ANGLE = wrapMinMax(REF_ANGLE, -pi, pi);
+    
+    REF_SPEED = 5;
+    REF_ASPEED = 1; 
 
     double ki = 0.87;
     double kd = 0.13;
@@ -129,12 +134,11 @@ std::pair<double, double> calculate(std::string team, int id, double x, double y
     double e2 = 0.0113 * difY * (cos(REF_ANGLE) - sin(REF_ANGLE));
     double e3 = wrapMinMax(REF_ANGLE - CUR_ANGLE, -pi, pi);
 
-    double d = distance(REF_X, REF_Y, CUR_X, CUR_Y);
 
-    double k = 1;
+    double k = 0.94;
 
-    if ((d <= 0.22 || REF_X == 12 || REF_X == 157 || REF_Y == -110 || REF_Y == -15) && id != 0)
-        k = 2;
+    if ((d <= 0.24 || REF_X == 12 || REF_X == 157 || REF_Y == -110 || REF_Y == -15) && id != 0)
+        k = 2.2;
 
     double v = REF_SPEED * cos(e3) + rob.k[0] * e1;
     double w = REF_ASPEED + k * rob.k[1] * REF_SPEED * e2 * +k * rob.k[2] * REF_SPEED * sin(e3);
@@ -142,7 +146,7 @@ std::pair<double, double> calculate(std::string team, int id, double x, double y
     double right = (v * 2.0 + w * rob.l) / (2.0 * rob.r);
     double left = (v * 2.0 - w * rob.l) / (2.0 * rob.r);
 
-    int newId = team == "Yellow" ? id : 3 + id;
+    int newId = team == "yellow" ? id : 3 + id;
 
     auto now = std::chrono::system_clock::now();
     double time = std::chrono::duration_cast<std::chrono::milliseconds>(now - rob.before[newId]).count();
@@ -159,20 +163,21 @@ std::pair<double, double> calculate(std::string team, int id, double x, double y
         rob.before[newId] = now;
 
         prop += errorD * kd + rob.errorG[newId] * ki;
-        std::cout << "prop: " << prop << std::endl;
+  //      std::cout << "prop: " << prop << std::endl;
         right *= prop;
         left *= prop;
     }
 
-    right = map(right, -s1, s1, -s2, s2);
-    left = map(left, -s1, s1, -s2, s2);
+    right = map(right, -rob.s1, rob.s1, -rob.s2, rob.s2);
+    left = map(left, -rob.s1, rob.s1, -rob.s2, rob.s2);
 
-    if (right > -min && right < min)
-        right = right < 0 ? -min : min;
+    if (right > -rob.min && right < rob.min)
+        right = right < 0 ? -rob.min : rob.min;
 
-    if (left > -min && left < min)
-        left = left < 0 ? -min : min;
+    if (left > -rob.min && left < rob.min)
+        left = left < 0 ? -rob.min : rob.min;
 
+ //   std::cout<<team << " "<<id<<" right: "<<right<<" left: "<<left<<std::endl;
     return std::make_pair(right, left);
 }
 
@@ -203,14 +208,16 @@ int main(int argc, char **argv)
 
         velocitiesYellow = {std::make_pair(0, 0), std::make_pair(0, 0), std::make_pair(0, 0)};
         velocitiesBlue = {std::make_pair(0, 0), std::make_pair(0, 0), std::make_pair(0, 0)};
+      
+       // sTeam, id, x, y, angle (if not -1000), vectorVelocities.
 
-        moveTo("yellow", 0, 158, state.ball.y < 46 ? 46 : state.ball.y > 86 ? 86 : state.ball.y, -100, velocitiesYellow);
-        moveTo("yellow", 0, state.ball.x, state.ball.y, -100, velocitiesYellow);
-        moveTo("yellow", 0, state.ball.x, state.ball.y, -100, velocitiesYellow);
+        moveTo("yellow", 0, 158, state.ball.y < 46 ? 46 : state.ball.y > 86 ? 86 : state.ball.y, -1000, velocitiesYellow);
+        moveTo("yellow", 1, state.ball.x, state.ball.y, -1000, velocitiesYellow);
+        moveTo("yellow", 2, state.ball.x, state.ball.y, -1000, velocitiesYellow);
 
-        moveTo("blue", 0, 10, state.ball.y < 46 ? 46 : state.ball.y > 86 ? 86 : state.ball.y, -100, velocitiesBlue);
-        moveTo("blue", 0, state.ball.x, state.ball.y, -100, velocitiesBlue);
-        moveTo("blue", 0, state.ball.x, state.ball.y, -100, velocitiesBlue);
+        moveTo("blue", 0, 10, state.ball.y < 46 ? 46 : state.ball.y > 86 ? 86 : state.ball.y, -1000, velocitiesBlue);
+        moveTo("blue", 1, state.ball.x, state.ball.y, -1000, velocitiesBlue);
+        moveTo("blue", 2, state.ball.x, state.ball.y, -1000, velocitiesBlue);
 
         send_commands(velocitiesYellow, velocitiesBlue);
     }
@@ -218,103 +225,23 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void send_commands(std::vector<std::pair<double, double>> velY, std::vector<std::pair<double, double>> velB, )
+void send_commands(std::vector<std::pair<double, double>> velY, std::vector<std::pair<double, double>> velB)
 {
     Command commandY;
     Command commandB;
 
     for (int i = 0; i < 3; i++)
-        commandY.commands.push_back(WheelsCommand(velY[i].first, velY[i].second));
-
+    commandY.commands.push_back(WheelsCommand(velY[i].first, velY[i].second));
+       
     commandSenderY->sendCommand(commandY);
 
     for (int i = 0; i < 3; i++)
-        commandB.commands.push_back(WheelsCommand(velB[i].first, velB[i].second));
-
+    commandB.commands.push_back(WheelsCommand(velB[i].first, velB[i].second));
+    
     commandSenderB->sendCommand(commandB);
+
+
 }
-
-std::pair<double, double> calculate(int id, position Cur, position Ref)
-{
-
-    double ki = 0.87;
-    double kd = 0.13;
-
-    double difX = Ref.x - Cur.x;
-    double difY = Ref.y - Cur.y;
-
-    if (difX == 0 && difY == 0)
-        Ref.angle = 0;
-    else if (difX == 0)
-        Ref.angle = pi / 2.0;
-    else
-        Ref.angle = atan(difY / difX);
-
-    if (Cur.x > Ref.x)
-        Ref.angle += pi;
-
-    Ref.angle = wrapMinMax(Ref.angle, -pi, pi);
-
-    //Ref.speed = Cur.speed<0? -5: 5;
-    Ref.x += rob.ts * Ref.speed * cos(Ref.angle);
-    Ref.y += rob.ts * Ref.speed * sin(Ref.angle);
-
-    Ref.x = Ref.x < 12 ? 12 : Ref.x > 157 ? 157 : Ref.x;
-    Ref.y = Ref.y > -15 ? -15 : Ref.y < -110 ? -110 : Ref.y;
-
-    difX = Ref.x - Cur.x;
-    difY = Ref.y - Cur.y;
-
-    double e1 = 0.0113 * difX * (cos(Ref.angle) + sin(Ref.angle));
-    double e2 = 0.0113 * difY * (cos(Ref.angle) - sin(Ref.angle));
-    double e3 = wrapMinMax(Ref.angle - Cur.angle, -pi, pi);
-
-    double d = distance(Ref.x, Ref.y, Cur.x, Cur.y);
-
-    double k = 1;
-    if ((d <= 0.22 || Ref.x == 12 || Ref.x == 157 || Ref.y == -110 || Ref.y == -15) && id != 0)
-        k = 2;
-
-    double v = Ref.speed * cos(e3) + rob.k[0] * e1;
-    double w = Ref.Aspeed + k * rob.k[1] * Ref.speed * e2 * +k * rob.k[2] * Ref.speed * sin(e3);
-
-    Cur.speed = v;
-    Cur.Aspeed = w;
-
-    double right = (Cur.speed * 2.0 + Cur.Aspeed * rob.l) / (2.0 * rob.r);
-    double left = (Cur.speed * 2.0 - Cur.Aspeed * rob.l) / (2.0 * rob.r);
-
-    auto now = std::chrono::system_clock::now();
-    double time = std::chrono::duration_cast<std::chrono::milliseconds>(now - rob.before[id]).count();
-    double prop = 1;
-    double errorD;
-
-    if (time > 180)
-    {
-        rob.errorG[id] = rob.errorG[id] * 0.8 + (d)*time * 0.001;
-        errorD = (d - rob.distanceA[id]) / (time * 0.001);
-
-        rob.distanceA[id] = d;
-        rob.before[id] = now;
-
-        prop += errorD * kd + rob.errorG[id] * ki;
-        std::cout << "prop: " << prop << std::endl;
-        right *= prop;
-        left *= prop;
-    }
-
-    right = map(right, -s1, s1, -s2, s2);
-    left = map(left, -s1, s1, -s2, s2);
-
-    if (right > -min && right < min)
-        right = right < 0 ? -min : min;
-
-    if (left > -min && left < min)
-        left = left < 0 ? -min : min;
-
-    return std::make_pair(right, left);
-}
-
 void send_debug()
 {
     vss::Debug debug;
